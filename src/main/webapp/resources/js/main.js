4cf3fa58430befc2aef273e83e992527f9a10cb6
IziKodzik <61714914+IziKodzik@users.noreply.github.com>
2020-09-07 10:19:41 +0200

    Tests

--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -2,14 +2,16 @@ console.log("qq");
 const button = document.querySelector("button");
 const test = document.getElementById("grooved-bearing-style");
 const agent = document.getElementById("bonding-agent");
+button.disabled=false;
+
 
 agent.addEventListener('change',function () {
-    if(agent.options[agent.selectedIndex].value === ("other")) {
-        test.disabled = true;
-    }else
-        test.disabled=false;
+
 })
 
-test.addEventListener('click',function () {
-    
+button.addEventListener('click',function () {
+    var index = 0;
+    if(test.selectedIndex === 0)
+        test.selectedIndex = 1;
+    test.options[index].disabled = !test.options[index].disabled;
 })
